cfdfc8b19e82b0abda3030dd9ebcec1adeede7fe
Ananga Rekha <anangarekhaa@gmail.com>
2026-03-01 14:31:01 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://learnsphere-backend.onrender.com",
+  baseURL: "https://learnsphere-rag.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
